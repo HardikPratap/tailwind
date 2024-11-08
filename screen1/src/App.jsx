d19c07571bb0fcd58337ad213242590c940463a3
Hardik Pratap <hardikpratap188@gmail.com>
2024-11-08 23:35:39 +0530

    update

--- screen1/src/App.jsx
+++ screen1/src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import Screen1 from './components/screen1'
+import Screen1 from "./components/screen1";
 import Screen2 from './components/screen2'
 import Otp from './components/Otp/Otp'
 import { BrowserRouter, Route,Routes } from 'react-router-dom'
