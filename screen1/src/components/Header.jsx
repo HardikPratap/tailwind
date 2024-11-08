1830d8e531fa1bbcfc604013a3664633a1dbbc36
Hardik Pratap <hardikpratap188@gmail.com>
2024-11-08 22:52:37 +0530

    assignment 1 done

--- screen1/src/components/Header.jsx
+++ screen1/src/components/Header.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
     <div className='flex flex-row justify-center gap-20 m-10 '>
+        <Link to={"/Screen1"}>
       <div className='bg-para px-3 py-2 rounded-md text-black font-semibold text-lg'>Date</div>
+      </Link>
+      <Link to={"/Screen2"}>
       <div className='bg-para px-3 py-2 rounded-md text-black font-semibold text-lg'>E-mail</div>
+      </Link>
+      <Link to={"/OTP"}>
+
       <div className='bg-para px-3 py-2 rounded-md text-black font-semibold text-lg'>OTP</div>
+      </Link>
     </div>
   )
 }
