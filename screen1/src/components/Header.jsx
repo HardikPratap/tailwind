c12632bb9281d3009884402af32153a502cc9599
Hardik Pratap <hardikpratap188@gmail.com>
2024-11-08 23:20:09 +0530

    update functions

--- screen1/src/components/Header.jsx
+++ screen1/src/components/Header.jsx
@@ -3,16 +3,19 @@ import { Link } from 'react-router-dom'
 
 function Header() {
   return (
-    <div className='flex flex-row justify-center gap-20 m-10 '>
+    <div className="flex flex-row  gap-16 m-10 rounded-xl bg-input text-gray-400 max-w-fit justify-self-center">
+        <Link to={"/"}>
+      <div className=' px-3 py-2 rounded-md  font-semibold text-lg'>Home</div>
+      </Link>
         <Link to={"/Screen1"}>
-      <div className='bg-para px-3 py-2 rounded-md text-black font-semibold text-lg'>Date</div>
+      <div className=' px-3 py-2 rounded-md  font-semibold text-lg'>Date</div>
       </Link>
       <Link to={"/Screen2"}>
-      <div className='bg-para px-3 py-2 rounded-md text-black font-semibold text-lg'>E-mail</div>
+      <div className=' px-3 py-2 rounded-md  font-semibold text-lg'>E-mail</div>
       </Link>
       <Link to={"/OTP"}>
 
-      <div className='bg-para px-3 py-2 rounded-md text-black font-semibold text-lg'>OTP</div>
+      <div className=' px-3 py-2 rounded-md  font-semibold text-lg'>OTP</div>
       </Link>
     </div>
   )
